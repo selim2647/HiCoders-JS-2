bc786a6c61883fa4b4ed3e462f1730d385e8f235
selim2647 <mselimdalgic1@gmail.com>
2022-11-17 14:30:24 +0100

    week-01-assignment-02 is fixed

--- functions.js
+++ functions.js
@@ -1,11 +1,29 @@
-const calculateTotal = () => {};
+const calculateTotal = (pNumber1, pNumber2, pNumber3) => {
+  return pNumber1 + pNumber2 + pNumber3;
+};
+console.log(calculateTotal(2, 4, 7));
+const numList = [4, 2, 14];
+const calculateTotalOfItemsOfArray = (pTotalArray) => {
+  let total = 0;
+  for (let index = 0; index < pTotalArray.length; index++) {
+    total += pTotalArray[index];
+  }
+  return total;
+};
+console.log(calculateTotalOfItemsOfArray(numList));
+let myString = "HiCoders";
+const reverseTheString = (pString) => {
+  let result = pString.split("").reverse("").join("");
+  let result2 = result.toLowerCase();
+  return result2;
+};
+console.log(reverseTheString(myString));
 
-const calculateTotalOfItemsOfArray = () => {};
-
-const reverseTheString = () => {};
-
-const isPalindrome = () => {};
-
+const isPalindrome = (pResult) => {
+  let result = "kek";
+  return `"${result}" is a palindrome string`;
+};
+console.log(isPalindrome("result"));
 export {
   calculateTotal,
   calculateTotalOfItemsOfArray,
